deda41dfbf86bf5d9aace23529ec7c672d474892
LuisVasconcelos0713 <Lgvs1307@gmail.com>
2026-07-14 23:49:44 -0300

    adding scripts `lint:check` and `lint:fix`

--- pages/compras/roupas.js
+++ pages/compras/roupas.js
@@ -1,9 +1,9 @@
 const roupas = () => {
-    return(
-        <div>
-            <h1>Roupas Section</h1>
-        </div>
-    )
-} 
+  return (
+    <div>
+      <h1>Roupas Section</h1>
+    </div>
+  );
+};
 
 export default roupas;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,5 @@
 const Home = () => {
-    return(
-        <h1>Teste543</h1>
-    )
-}
+  return <h1>Teste543</h1>;
+};
 
 export default Home;
