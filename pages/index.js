4eb94a2b1f5099faff814ab04092d70817f4bc91
LuisVasconcelos0713 <Lgvs1307@gmail.com>
2026-07-21 20:50:19 -0300

    feat: modifing initial phrase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 const Home = () => {
-  return <h1>Teste543</h1>;
+  return (
+    <h1>
+      Todo grande projeto precisa de uma inspiração. A minha sempre será você.
+      Te amo luisa ❤️
+    </h1>
+  );
 };
 
 export default Home;
