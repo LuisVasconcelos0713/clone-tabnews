478192ea15da883c9dd3b9e48928000f6cad7197
LuisVasconcelos0713 <Lgvs1307@gmail.com>
2026-07-25 00:10:38 -0300

    feat: adding compose.yaml file

--- tests/Integration/api/v1/status/get.test.js
+++ tests/Integration/api/v1/status/get.test.js
@@ -3,3 +3,9 @@ test("GET to /api/v1/status, should return 200", async () => {
   console.log(response);
   expect(response.status).toBe(200);
 });
+
+test("GET should return, error 404", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status/teste");
+  console.log(response.status);
+  expect(response.status).toBe(404);
+});
