f1ea9aa85ef2cdeccd1dd09fa93d8a06c938f889
Luis Gustavo Vasconcelos <83058118+LuisVasconcelos0713@users.noreply.github.com>
2026-07-26 15:01:33 +0000

    feat: adding .env file~

--- infra/database.js
+++ infra/database.js
@@ -2,11 +2,11 @@ import {Client} from 'pg'
 
 const query = async (queryObjetct) => {
  const client = new Client({
-    host: "localhost",
-    port: 5432,
-    user:"postgres",
-    database:"postgres",
-    password:"Local_Password",
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
  })
  await client.connect()
  const result = await client.query(queryObjetct)
